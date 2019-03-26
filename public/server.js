d0dfc93e3bf05b105c20b496771080d7fc6e67ca
OZONE4REAL <ezenwaogbonna1@gmail.com>
2019-03-26 11:57:49 +0100

    ch: modify webpack config

--- public/server.js
+++ public/server.js
@@ -16,11 +16,11 @@ var port = process.env.PORT || 3000; // __dirname = /
 // react  app
 // resolve = /dist
 
-app.use(_express.default.static(_path.default.resolve(__dirname, '../dist'))); // handling react router, for request to react app
+app.use(_express.default.static(_path.default.resolve(__dirname, 'dist'))); // handling react router, for request to react app
 // join = /dist/index.html
 
 app.get('*', function (req, res) {
-  res.sendFile(_path.default.join(__dirname, '../dist/index.html'));
+  res.sendFile(_path.default.join(__dirname, 'dist/index.html'));
 });
 app.listen(port, function () {
   return console.log('server running at port', port);
