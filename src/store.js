4b76e5bec434ec976e14f733ff212331c1a87576
OZONE4REAL <ezenwaogbonna1@gmail.com>
2019-03-30 06:50:00 +0100

    ch: modify redux configuration

--- src/store.js
+++ src/store.js
@@ -1,16 +1,14 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 const middleware = [thunk];
 const initialState = {};
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeWithDevTools(applyMiddleware(...middleware)),
 );
 
 export default store;
